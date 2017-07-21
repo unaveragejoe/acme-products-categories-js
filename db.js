df9f9166b6846a4b142499f71ab502318290ed49
Joseph Kholodenko <joekho@joekho-macbookpro2.roam.corp.google.com>
2017-07-21 16:31:49 -0400

    first commit

---
+++ db.js
@@ -0,0 +1,37 @@
+// - store data in an object literal
+// - export
+//   - getCategoryNames
+//   - getProductsByCategory
+//   - createProduct
+//   - deleteProduct
+//   - updateProduct
+//   - deleteCategory
+//   - createCategory
+    
+
+var data = 
+{
+  "Bar Category": [
+    {
+      "name": "bar 1",
+      "id": 1
+    }
+  ],
+  "": [],
+  "Foo Category": [
+    {
+      "name": "foo 1",
+      "id": 1
+    },
+    {
+      "name": "foo 2",
+      "id": 2
+    }
+  ],
+  "Sports - Baseball": [
+    {
+      "name": "Wilson hardball bat",
+      "id": 1
+    }
+  ]
+};
---
+++ routes/categories.js
@@ -0,0 +1,8 @@
+// /routes/categories.js (~30 loc)
+// - 5 route total
+// - GET /categories/:name/products
+// - POST /categories
+// - DELETE /categories/:name
+// - POST /categories/:name/products/
+// - DELETE /categories/:name/products/:id
+    
